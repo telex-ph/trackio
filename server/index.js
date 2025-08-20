a462ffa3933f8dea7148f1f248e90e1d6bc7f813
domingohernani <dev.hernani.domingo@gmail.com>
2025-08-20 13:03:56 +0800

    feat: setup server with axios, dotenv, test endpoint, and .gitignore

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log("helloos");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+
+app.use(cors({ origin: ["http://localhost:5173"] }));
+
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: "gumagana" });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Listening to PORT ${PORT}`);
+});
