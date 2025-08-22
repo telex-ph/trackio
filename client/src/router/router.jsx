87d4a686ffe90c470a4ad3275bb138326a195991
domingohernani <dev.hernani.domingo@gmail.com>
2025-08-22 17:50:34 +0800

    fixed: move the login route outside of the App route

--- client/src/router/router.jsx
+++ client/src/router/router.jsx
@@ -7,13 +7,13 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
+    path: "login",
+    element: <Login />,
+  },
+  {
     path: "/",
     element: <App />,
     children: [
-      {
-        path: "login",
-        element: <Login />,
-      },
       {
         path: "/",
         element: <AppLayout />,
