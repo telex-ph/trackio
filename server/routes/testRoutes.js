90b63efc3c2750a89fcf7dab3734b16e0190c56a
domingohernani <dev.hernani.domingo@gmail.com>
2025-08-26 13:52:35 +0800

    chore: deleted test codes

--- server/routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { test } from "../controllers/testCotroller.js";
-import { verifyJWT } from "../middlewares/verifyJWT.js";
-import { handleExpiredToken } from "../middlewares/handleExpiredToken.js";
-const router = Router();
-
-router.get("/test", verifyJWT, handleExpiredToken, test);
-
-export default router;
