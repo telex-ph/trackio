b3d84cb3d4ae8d9a08bddd11ca89aba6f8aaf974
domingohernani <dev.hernani.domingo@gmail.com>
2025-08-27 16:03:53 +0800

    feat: implement Table reusuable component

---
+++ client/src/components/Table.jsx
@@ -0,0 +1,45 @@
+import React, { useMemo, useRef } from "react";
+import { AgGridReact } from "@ag-grid-community/react";
+import { ModuleRegistry } from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+
+import "@ag-grid-community/styles/ag-grid.css";
+import "@ag-grid-community/styles/ag-theme-quartz.css";
+
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+const Table = ({ data, columns }) => {
+  const gridRef = useRef();
+
+  const defaultColDef = useMemo(
+    () => ({
+      filter: true,
+      sortable: true,
+      floatingFilter: true,
+    }),
+    []
+  );
+
+  const rowData = useMemo(() => data, [data]);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <div
+        className="ag-theme-quartz"
+        style={{ height: "90vh", width: "100%" }}
+      >
+        <AgGridReact
+          ref={gridRef}
+          rowData={rowData}
+          columnDefs={columns}
+          defaultColDef={defaultColDef}
+          pagination={true}
+          paginationPageSize={15}
+          paginationPageSizeSelector={[15, 25, 50]}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Table;
--- client/src/pages/Tracking.jsx
+++ client/src/pages/Tracking.jsx
@@ -1,7 +1,57 @@
-const Tracking = () => {
-  return (
-    <div>Tracking</div>
-  )
-}
+import Table from "../components/Table";
 
-export default Tracking
+const Tracking = () => {
+  const data = [
+    { id: 1, name: "John Doe", email: "john@example.com", role: "Admin" },
+    { id: 2, name: "Jane Smith", email: "jane@example.com", role: "User" },
+    { id: 3, name: "Mark Lee", email: "mark@example.com", role: "Manager" },
+    { id: 1, name: "John Doe", email: "john@example.com", role: "Admin" },
+    { id: 2, name: "Jane Smith", email: "jane@example.com", role: "User" },
+    { id: 3, name: "Mark Lee", email: "mark@example.com", role: "Manager" },
+    { id: 1, name: "John Doe", email: "john@example.com", role: "Admin" },
+    { id: 2, name: "Jane Smith", email: "jane@example.com", role: "User" },
+    { id: 3, name: "Mark Lee", email: "mark@example.com", role: "Manager" },
+    { id: 1, name: "John Doe", email: "john@example.com", role: "Admin" },
+    { id: 2, name: "Jane Smith", email: "jane@example.com", role: "User" },
+    { id: 3, name: "Mark Lee", email: "mark@example.com", role: "Manager" },
+    { id: 1, name: "John Doe", email: "john@example.com", role: "Admin" },
+    { id: 2, name: "Jane Smith", email: "jane@example.com", role: "User" },
+    { id: 3, name: "Mark Lee", email: "mark@example.com", role: "Manager" },
+    { id: 1, name: "John Doe", email: "john@example.com", role: "Admin" },
+    { id: 2, name: "Jane Smith", email: "jane@example.com", role: "User" },
+    { id: 3, name: "Mark Lee", email: "mark@example.com", role: "Manager" },
+  ];
+
+  const columns = [
+    { headerName: "ID", field: "id", sortable: true, filter: true, flex: 1 },
+    {
+      headerName: "Name",
+      field: "name",
+      sortable: true,
+      filter: true,
+      flex: 2,
+    },
+    {
+      headerName: "Email",
+      field: "email",
+      sortable: true,
+      filter: true,
+      flex: 2,
+    },
+    {
+      headerName: "Role",
+      field: "role",
+      sortable: true,
+      filter: true,
+      flex: 1,
+    },
+  ];
+
+  return (
+    <>
+      <Table data={data} columns={columns} />
+    </>
+  );
+};
+
+export default Tracking;
