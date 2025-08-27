ec95dfa2673d026a02fe572bd2ac1fe0541f55ae
domingohernani <dev.hernani.domingo@gmail.com>
2025-08-27 10:41:16 +0800

    feat: implement server setup and middleware configuration in app.js

---
+++ server/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// Routes imports
+import authRoutes from "./routes/authRoutes.js";
+import accountRoutes from "./routes/accountRoutes.js";
+
+const app = express();
+dotenv.config();
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/accounts", accountRoutes);
+
+export default app;
