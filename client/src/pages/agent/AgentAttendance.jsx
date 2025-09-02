62f5b554ea29caa404704ac07c38694e616fd729
domingohernani <dev.hernani.domingo@gmail.com>
2025-09-02 16:20:02 +0800

    cgore: initial agents user interface

--- client/src/pages/agent/AgentAttendance.jsx
+++ client/src/pages/agent/AgentAttendance.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 const AgentAttendance = () => {
-  return <div>AgentAttendance</div>;
+  return (
+    <div>
+      <section className="flex gap-5">
+        <section className="flex items-center flex-1 p-5 container-light border-light rounded-md">
+          <div className="flex-1">
+            <h1>WORKING HOURS COUNTER</h1>
+          </div>
+          <div className="grid grid-cols-3 gap-2 [&>*]:text-center">
+            <span>
+              <h1 className="bg-white border-light rounded-md">01</h1>
+              <span>Hours</span>
+            </span>
+            <span>
+              <h1 className="bg-white border-light rounded-md">28</h1>
+              <span>Minutes</span>
+            </span>
+            <span>
+              <h1 className="bg-white border-light rounded-md">42</h1>
+              <span>Seconds</span>
+            </span>
+          </div>
+        </section>
+        <section className="flex items-center flex-1 p-5 container-light border-light rounded-md">
+          <div className="flex-1">
+            <h1>SERVER TIME</h1>
+          </div>
+          <div className="grid grid-cols-3 gap-2 [&>*]:text-center">
+            <span>
+              <h1 className="bg-white border-light rounded-md">01</h1>
+              <span>Hours</span>
+            </span>
+            <span>
+              <h1 className="bg-white border-light rounded-md">28</h1>
+              <span>Minutes</span>
+            </span>
+            <span>
+              <h1 className="bg-white border-light rounded-md">42</h1>
+              <span>Seconds</span>
+            </span>
+          </div>
+        </section>
+      </section>
+    </div>
+  );
 };
 
 export default AgentAttendance;
