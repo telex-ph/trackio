56d05fbff05c901bb73dbaf845e886de502a8efb
domingohernani <dev.hernani.domingo@gmail.com>
2025-09-02 16:19:23 +0800

    chore: adjust app layout

--- client/src/layout/AppLayout.jsx
+++ client/src/layout/AppLayout.jsx
@@ -5,13 +5,13 @@ import Topbar from "./Topbar";
 export default function AppLayout() {
   return (
     <div className="h-screen grid grid-cols-[auto_1fr] grid-rows-[60px_1fr] max-w-[200rem] mx-auto">
-      <section className="col-start-2 col-end-3 row-start-1 row-end-2 border-b border-gray-200">
+      <section className="col-start-1 col-end-3 row-start-1 row-end-2 border-b border-gray-200">
         <Topbar />
       </section>
-      <section className="col-start-1 col-end-2 row-start-1 row-end-3 border-r border-l border-gray-200">
+      <section className="col-start-1 col-end-2 row-start-2 row-end-3 border-r border-l border-gray-200">
         <Sidebar />
       </section>
-      <main className="">
+      <main className="p-5">
         <Outlet />
       </main>
     </div>
