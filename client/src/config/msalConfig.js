baee97a9a2b6eb9056fd9df56c7ac01aeb4aa803
LEVi19xx <charmainejoycecoloma02@gmail.com>
2025-09-17 03:17:35 +0800

    chore: redirect url for ms account

--- client/src/config/msalConfig.js
+++ client/src/config/msalConfig.js
@@ -4,7 +4,7 @@ const msalConfig = {
   auth: {
     clientId: "ce1ef47d-471d-4c3b-a3c5-f9b9e0ac1ea1",
     authority: "https://login.microsoftonline.com/common",
-    redirectUri: "http://localhost:5173/dashboard",
+    redirectUri: "https://trackio-a0um.onrender.com/dashboard",
   },
 };
 export const msalInstance = new PublicClientApplication(msalConfig);
