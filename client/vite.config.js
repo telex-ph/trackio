8af1c7675c682bae027b3dfcdd591c835642a01c
LEVi19x <charmainejoycecoloma02@gmail.com>
2025-09-16 15:12:05 +0800

    chore: update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,16 +3,11 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import flowbiteReact from 'flowbite-react/plugin/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
   preview: {
-    host: '0.0.0.0', // allows network access
-    port: parseInt(process.env.PORT) || 4173, // use Render's $PORT
-    allowedHosts: 'all' // allow all hosts to prevent "Blocked request" errors
+    host: '0.0.0.0', // already there
+    port: parseInt(process.env.PORT) || 4173,
+    allowedHosts: ['trackio-1.onrender.com'],
   },
-  build: {
-    outDir: 'dist', // production-ready files go here
-    base: '/' // base path
-  }
 });
