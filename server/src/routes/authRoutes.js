a668e9356166846280be652f78906790b0f1f11e
LEVi19xx <charmainejoycecoloma02@gmail.com>
2025-09-18 00:26:56 +0800

    chore: bug fixingfor login

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -7,7 +7,7 @@ import {
   getStatus,
   login,
 } from "../controllers/authControllers.js";
-//import { verifyJWT } from "../middlewares/verifyJWT.js";
+import { verifyJWT } from "../middlewares/verifyJWT.js";
 // import { handleExpiredToken } from "../middlewares/handleExpiredToken.js";
 const router = Router();
 
