d8f746be43c07e6f5764cf3c792dee1af1da277f
domingohernani <dev.hernani.domingo@gmail.com>
2025-09-18 15:02:25 +0800

    chore: cron job schedule adjusted

--- server/src/cron.js
+++ server/src/cron.js
@@ -1,10 +1,10 @@
 import cron from "node-cron";
 import { checkAbsences, markAbsences } from "./services/absence.services.js";
 
-// Runs every 1 minute
+// Runs every 1hr
 cron.schedule("* * * * *", async () => {
-  const users = await checkAbsences();
-  if (users) {
-    await markAbsences(users);
+  const absentees = await checkAbsences();
+  if (absentees) {
+    await markAbsences(absentees);
   }
 });
