db2f1960c0ce24dc8baa8f13cab254bdbea4cc6e
domingohernani <dev.hernani.domingo@gmail.com>
2025-09-19 17:36:05 +0800

    test: test upload multiple files on cloudinary

--- client/src/pages/om/OMDashboard.jsx
+++ client/src/pages/om/OMDashboard.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import api from "../../utils/axios";
+import { useState } from "react";
 
 const OMDashboard = () => {
-  return <div>OMDashboard</div>;
+  const [files, setFiles] = useState([]);
+
+  const handleFileChange = (e) => {
+    setFiles(Array.from(e.target.files));
+  };
+
+  const uploadMedia = async () => {
+    try {
+      const formData = new FormData();
+
+      files.forEach((file) => formData.append("files", file));
+
+      const response = await api.post("/media/upload-media", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        multiple
+        onChange={handleFileChange}
+        className="bg-red-300 border"
+      />
+      Testing Cloudinary
+      <button onClick={uploadMedia} className="bg-red-300">
+        Upload
+      </button>
+    </div>
+  );
 };
 
 export default OMDashboard;
