c39bdef179fb013ada1faa71a1d6691227b8b98b
LEVi99xx <charmainejpycecoloma02@gmail.com>
2025-09-22 17:15:37 +0800

    feat(admin,tl): enable Request pages with DB integration

---
+++ server/src/routes/requestRoutes.js
@@ -0,0 +1,51 @@
+// src/routes/requestRoutes.js
+import express from "express";
+const router = express.Router();
+
+import {
+  addRequest,
+  getRequests,
+  getRequestById,
+  updateRequest,
+  deleteRequest,
+  getRequestsByAgent,
+  getRequestsBySupervisor,
+  getRequestsByStatus,
+  getRequestsByType,
+  approveRequest,  
+  rejectRequest,
+} from "../controllers/requestControllers.js";
+
+// GET all
+router.get("/", getRequests);
+
+// GET by ID
+router.get("/:id", getRequestById);
+
+// GET by agent
+router.get("/agent/:agentName", getRequestsByAgent);
+
+// GET by supervisor
+router.get("/supervisor/:supervisor", getRequestsBySupervisor);
+
+// GET by status
+router.get("/status/:status", getRequestsByStatus);
+
+// GET by type
+router.get("/type/:type", getRequestsByType);
+
+// POST new
+router.post("/", addRequest);
+
+// UPDATE existing
+router.put("/:id", updateRequest);
+
+// DELETE
+router.delete("/:id", deleteRequest);
+
+router.patch("/:id/approve", approveRequest);
+
+router.patch("/:id/reject", rejectRequest);
+
+
+export default router;
