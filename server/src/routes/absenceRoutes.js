b51c51b3d7fa938b4c12221a6e2ca3c5782c7bf5
domingohernani <dev.hernani.domingo@gmail.com>
2025-09-22 11:17:54 +0800

    feat: add absence marking controller and route

--- server/src/routes/absenceRoutes.js
+++ server/src/routes/absenceRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/verifyJWT.js";
-import { getAbsentees } from "../controllers/absenceController.js";
+import {
+  addAbsentees,
+  getAbsentees,
+} from "../controllers/absenceController.js";
 const router = Router();
 
 router.get("/get-absentees", verifyJWT, getAbsentees);
+
+router.get("/add-absentees", addAbsentees);
 
 export default router;
