cd6127ebde55e6ff5da2fd6ffcfd46ec42e9fffe
domingohernani <dev.hernani.domingo@gmail.com>
2025-09-24 10:24:17 +0800

    style: changed border styling

--- client/src/layout/AppLayout.jsx
+++ client/src/layout/AppLayout.jsx
@@ -29,18 +29,16 @@ export default function AppLayout() {
 
   return (
     <div className="h-screen grid grid-cols-[auto_1fr] grid-rows-[60px_1fr] max-w-[200rem] mx-auto">
-      <section className="col-start-1 col-end-3 row-start-1 row-end-2 border-b border-gray-300">
+      <section className="col-start-1 col-end-3 row-start-1 row-end-2 border-b border-gray-200">
         <Topbar
           toggleSidebar={toggleSidebar}
           isCollapsed={isSidebarCollapsed}
         />
       </section>
-      <section className="col-start-1 col-end-2 row-start-2 row-end-3 border-r border-gray-300 overflow-y-auto">
+      <section className="col-start-1 col-end-2 row-start-2 row-end-3 border-r border-gray-200 overflow-y-auto">
         <Sidebar isCollapsed={isSidebarCollapsed} />
       </section>
-      <main
-        className="p-5 overflow-y-auto container-light"
-      >
+      <main className="p-5 overflow-y-auto">
         <Outlet />
       </main>
     </div>
