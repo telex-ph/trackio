d2ad55ff2b61598e5507f1fde27312ab2dabee34
domingohernani <dev.hernani.domingo@gmail.com>
2025-09-24 17:37:29 +0800

    feat: add custom hook for pressing shift key during scheduling management

---
+++ client/src/hooks/useShiftKey.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+const useShiftKey = () => {
+  const [isShiftPressed, setIsShiftPressed] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Shift") setIsShiftPressed(true);
+    };
+
+    const handleKeyUp = (e) => {
+      if (e.key === "Shift") setIsShiftPressed(false);
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
+  return isShiftPressed;
+};
+
+export default useShiftKey;
