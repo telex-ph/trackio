34a57fa8756c6a00146dbb15fa986d7f82c3b2b1
domingohernani <dev.hernani.domingo@gmail.com>
2025-09-26 18:00:38 +0800

    wip: added date formatted

--- client/src/utils/formatDateTime.js
+++ client/src/utils/formatDateTime.js
@@ -1,21 +1,29 @@
 import { DateTime } from "luxon";
+
 const fmtTime = "hh:mm a";
 const fmtDate = "yyyy-MM-dd";
 const zone = "Asia/Manila";
 
+// TODO: make this somehow consistent
 export const formatTime = (time) => {
   if (!time) return "---";
-
-  const formattedTime = time
-    ? DateTime.fromISO(time).setZone(zone).toFormat(fmtTime)
-    : "Not Logged In";
-
-  return formattedTime;
+  return DateTime.fromISO(time).setZone(zone).toFormat(fmtTime);
 };
 
 export const formatDate = (date) => {
-  const formatDate = date
-    ? DateTime.fromISO(date).setZone(zone).toFormat(fmtDate)
-    : "Not Logged In";
-  return formatDate;
+  if (!date) return "---";
+  return DateTime.fromISO(date).setZone(zone).toFormat(fmtDate);
+};
+
+export const toDateTimeFromTimeString = (timeStr) => {
+  if (!timeStr) return null;
+
+  const today = DateTime.now().setZone(zone).startOf("day");
+  const [hour, minute] = timeStr.split(":").map(Number);
+
+  return today.set({ hour, minute });
+};
+
+export const dateFormatter = (date, format) => {
+  return DateTime.fromISO(date).setZone(zone).toFormat(format);
 };
