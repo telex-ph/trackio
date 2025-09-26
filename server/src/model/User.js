5fa6af7f3483bcf0e4d8911a55c2871ec27a37b5
domingohernani <dev.hernani.domingo@gmail.com>
2025-09-26 17:58:09 +0800

    feat: added getUserbyId in User class

--- server/src/model/User.js
+++ server/src/model/User.js
@@ -1,8 +1,20 @@
 import connectDB from "../config/db.js";
 import { DateTime } from "luxon";
+import { ObjectId } from "mongodb";
 
 class User {
   static #collection = "users";
+
+  static async getById(id) {
+    if (!id) {
+      throw new Error("ID is required");
+    }
+
+    const db = await connectDB();
+    const collection = db.collection(this.#collection);
+    const user = await collection.findOne({ _id: new ObjectId(id) });
+    return user;
+  }
 
   static async getAll() {
     const db = await connectDB();
