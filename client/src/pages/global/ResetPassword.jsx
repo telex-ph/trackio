599b51844ed88ff4f3f36bf592d1c8d3d98c3371
domingohernani <dev.hernani.domingo@gmail.com>
2025-10-08 10:38:40 +0800

    refactor: remove funnymeme image from ResetPassword component

--- client/src/pages/global/ResetPassword.jsx
+++ client/src/pages/global/ResetPassword.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const ResetPassword = () => {
-  return (
-    <div className="w-fit mx-auto">
-      <img src="/funnymeme.jpeg" alt="" srcset="" />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default ResetPassword;
