3c0de0571e7657efadf2e2050914a27de59e7ef3
domingohernani <dev.hernani.domingo@gmail.com>
2025-10-17 14:53:22 +0800

    wip: socketio initial communication

--- client/src/pages/global/LiveMonitoring.jsx
+++ client/src/pages/global/LiveMonitoring.jsx
@@ -3,62 +3,40 @@ import { io } from "socket.io-client";
 import api from "../../utils/axios";
 import { AnimatePresence, motion } from "framer-motion";
 
-const URL = import.meta.env.VITE_API_RENDER_BASE_URL;
-const socket = io(URL, { transports: ["websocket", "polling"] });
-
 export default function LiveBreaks() {
-  const [attendances, setAttendances] = useState([]);
+  const [message, setMessage] = useState("");
 
-  const sortAttendance = (attendance) => {
-    const sortedAttendance = [...attendance].sort(
-      (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
-    );
-    setAttendances(sortedAttendance);
+  const socket = io("http://localhost:3000");
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("statuses", message);
   };
 
   useEffect(() => {
-    socket.on("attendances-updated", sortAttendance);
-    return () => socket.off("attendances-updated", sortAttendance);
-  }, []);
-
-  useEffect(() => {
-    const fetchAttendance = async () => {
-      try {
-        const { data } = await api.get("/attendance/get-all-onbreak");
-        sortAttendance(data);
-      } catch (error) {
-        console.error(error);
-      }
+    const handleStatuses = (msg) => {
+      console.log(msg);
     };
-    fetchAttendance();
+
+    socket.on("statuses", handleStatuses);
+
+    return () => {
+      socket.off("statuses", handleStatuses);
+    };
   }, []);
 
   return (
     <div className="p-4">
-      <div className="flex flex-col gap-3 rounded-md">
-        <AnimatePresence>
-          {attendances.map((attendance) => (
-            <motion.div
-              key={attendance._id + attendance.updatedAt}
-              layout
-              animate={{ x: 0, opacity: 1 }}
-              exit={{ x: 100, opacity: 0 }}
-              transition={{ type: "spring", stiffness: 300, damping: 30 }}
-            >
-              <p className="h-20 bg-(--primary-color) flex items-center justify-center rounded-md text-white">
-                {attendance.firstName} {attendance.lastName}
-              </p>
-              {/* <div className="flex flex-col mt-2">
-                {attendance.breaks?.map((brk, index) => (
-                  <span key={index} className="text-sm text-gray-700">
-                    Break {index + 1}: {brk.startTime} - {brk.endTime}
-                  </span>
-                ))}
-              </div> */}
-            </motion.div>
-          ))}
-        </AnimatePresence>
-      </div>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setMessage(e.target.value)}
+          className="container-light border-light"
+        />
+        <button type="submit" className="bg-red-300">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
