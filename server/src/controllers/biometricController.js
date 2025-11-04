8af1b901798d41134fd7da191f7c9aa468e7c882
domingohernani <dev.hernani.domingo@gmail.com>
2025-11-04 17:55:51 +0800

    testing logs

--- server/src/controllers/biometricController.js
+++ server/src/controllers/biometricController.js
@@ -47,7 +47,7 @@ export const getEvents = async (req, res) => {
                     }
                     lastEventMap.set(ac.employeeNoString, now);
 
-                    console.log(ac);
+                    console.log(event);
 
                     if (ipAddress === BIO_IP.ADMINDOOR) return res.status(200).send('OK');
 
