20f8bc3fd29042250f489890cdaa3908a7c474aa
domingohernani <dev.hernani.domingo@gmail.com>
2025-11-06 08:42:28 +0800

    account creation on ticketing

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -8,11 +8,49 @@ export const useAuth = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
+  const bearerToken = "Bearer standard_077ed3b9b01c0863d40827030797f5e602b32b89fe2f3f94cc495b475802c16512013466aaf82efa0d966bff7d6cf837d00e0bfdc9e91f4f290e893ba28c4d6330310f6428f7befc9ad39cd5a55f3b3ba09822aed74a922bf1e6ca958b2f844fab5259c0d69318160bb91d4ab2bf2bec0c72f6d94bf0666a59559c3992aa8b47";
+
+  const createAccountTicket = async (name, email) => {
+    try {
+      const url = "https://ticketing-system-eight-kappa.vercel.app/api/auth/register/trackio";
+
+      const body = {
+        name: name,
+        email: email,
+        password: "password",
+        role: "MANAGEMENT",
+      };
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: bearerToken,
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Account created:", data);
+      return data;
+    } catch (error) {
+      console.error("Error creating account ticket:", error);
+    }
+  };
+
   const login = async (data) => {
     try {
       const response = await api.post("/auth/log-in", data);
+      // await createAccountTicket(data);
       const user = response.data;
       if (user) {
+        const name = `${user.firstName} ${user.lastName}`;
+        const email = user.email;
+        await createAccountTicket(name, email);
         const loginResponse = await api.post("/auth/create-token", user);
         if (loginResponse.status === 200) {
           navigate(`/${user.role}/dashboard`);
--- server/constants/biometricsIp.js
+++ server/constants/biometricsIp.js
@@ -1,5 +1,5 @@
 export const BIO_IP = Object.freeze({
-    INDOOR: "192.168.100.29",
-    OUTDOOR: "192.168.100.30",
+    INDOOR: "192.168.3.6",
+    OUTDOOR: "192.168.3.7",
     ADMINDOOR: "192.168.100.31"
 });
