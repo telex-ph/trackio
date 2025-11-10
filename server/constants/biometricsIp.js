242e71e6725505ba734b6efbcb58f346798b2a10
domingohernani <dev.hernani.domingo@gmail.com>
2025-11-10 10:43:41 +0800

    biometric ips

--- server/constants/biometricsIp.js
+++ server/constants/biometricsIp.js
@@ -1,5 +1,5 @@
 export const BIO_IP = Object.freeze({
-    INDOOR: "192.168.3.6",
-    OUTDOOR: "192.168.3.7",
-    ADMINDOOR: "192.168.3.5"
+    INDOOR: "192.168.0.3",
+    OUTDOOR: "192.168.0.4",
+    ADMINDOOR: "192.168.0.2"
 });
