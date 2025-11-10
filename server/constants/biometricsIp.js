89c520cc816cfaa6d3364427d76230abab71b4b8
domingohernani <dev.hernani.domingo@gmail.com>
2025-11-10 17:58:57 +0800

    fix: updated biometrics ip address

--- server/constants/biometricsIp.js
+++ server/constants/biometricsIp.js
@@ -1,5 +1,9 @@
 export const BIO_IP = Object.freeze({
-    INDOOR: "192.168.0.3",
-    OUTDOOR: "192.168.0.4",
-    ADMINDOOR: "192.168.0.2"
+    INDOOR: "192.168.0.6",
+    OUTDOOR: "192.168.0.7",
+    ADMINDOOR: "192.168.0.5"
 });
+
+// ADMIN 0.5
+// IN 0.6
+// Chief 0.7
