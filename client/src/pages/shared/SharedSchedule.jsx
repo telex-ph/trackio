118b43ccd136b4c05be32c6765e46ed75b6ac863
domingohernani <dev.hernani.domingo@gmail.com>
2025-11-18 13:48:15 +0800

    refactor: add name fields instead of firstName and lastName

--- client/src/pages/shared/SharedSchedule.jsx
+++ client/src/pages/shared/SharedSchedule.jsx
@@ -39,8 +39,7 @@ const SharedSchedule = ({ role }) => {
   // Columns for Frontoffice (without department)
   const frontofficeColumns = [
     { headerName: "ID", field: "employeeId", flex: 1 },
-    { headerName: "First Name", field: "firstName", flex: 1 },
-    { headerName: "Last Name", field: "lastName", flex: 1 },
+    { headerName: "Name", field: "name", flex: 1 },
     { headerName: "Email", field: "email", flex: 1 },
     {
       headerName: "Upcoming Schedules",
