eb40669b174f3651215d796b630ee4669d4c11bb
Jayro Gabriel <gabriel.jayro26@gmail.com>
2025-12-03 11:10:48 +0800

    fix: mispelled imports

--- server/src/sockets/socket.js
+++ server/src/sockets/socket.js
@@ -27,10 +27,9 @@ const socket = async (server, app) => {
   app.set("io", io);
 
   io.on("connection", (socket) => {
-    console.log("Client connected:", socket.id);
-
+    // console.log("Client connected:", socket.id);
     socket.on("disconnect", () => {
-      console.log("Client disconnected:", socket.id);
+      // console.log("Client disconnected:", socket.id);
     });
   });
 
