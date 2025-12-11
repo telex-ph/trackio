fa2b313b572cee90cecfc3544409956c3f3b0681
Jayro Gabriel <gabriel.jayro26@gmail.com>
2025-12-11 16:58:26 +0800

    feat: update leave API and added edit and delete

--- server/src/routes/leaveRoutes.js
+++ server/src/routes/leaveRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyJWT as auth } from "../middlewares/verifyJWT.js";
-import { addLeave, getLeaveById, getLeaves } from "../controllers/leaveController.js";
+import { addLeave, deleteLeave, getLeaveById, getLeaves, updateLeave } from "../controllers/leaveController.js";
 
 const router = express.Router();
 
@@ -8,5 +8,9 @@ router.post("/", auth, addLeave);
 
 router.get("/", auth, getLeaves);
 router.get("/:id", auth, getLeaveById);
+
+router.put("/:id", auth, updateLeave);
+
+router.delete("/:id", auth, deleteLeave);
 
 export default router;
