1aad9ba3fb56990caec8f792515119d961c6cfe8
Jayro Gabriel <gabriel.jayro26@gmail.com>
2025-12-16 14:33:34 +0800

    fix: fix controller bug

--- server/src/controllers/userControllers.js
+++ server/src/controllers/userControllers.js
@@ -115,24 +115,19 @@ export const getUsersByRoleScope = async (req, res) => {
 };
 
 export const getUsersByAccount = async (req, res) => {
-  const userId = req.user?._id;
-
-  if (!userId) {
-    return res.status(400).json({ message: "User ID missing" });
-  }
+  const id = req.params.id;
 
   try {
-    const users = await User.getUserAccountsById(userId);
+    const users = await User.getUserAccountsById(id);
     res.status(200).json(users);
   } catch (error) {
-    console.error("Error fetching users by account scope:", error);
+    console.error("Error fetching users base on account scope: ", error);
     res.status(500).json({
       message: "Failed to fetch list of users by account scope",
       error: error.message,
     });
   }
 };
-
 
 export const deleteUser = async (req, res) => {
   const id = req.params.id;
